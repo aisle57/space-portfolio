049fc4caf9a35682d57e69f3cc446e9cf6d2906c
aisle57 <truistdesigns@gmail.com>
2026-07-28 14:22:40 -0400

    Change video source to Isotope-bg.mp4

--- components/main/hero.tsx
+++ components/main/hero.tsx
@@ -7,9 +7,10 @@ export const Hero = () => {
         autoPlay
         muted
         loop
+        playsInline
         className="rotate-180 absolute top-[-340px] left-0 w-full h-full object-cover -z-20"
       >
-        <source src="/videos/blackhole.webm" type="video/webm" />
+        <source src="/videos/Isotope-bg.mp4" type="video/mp4" />
       </video>
 
       <HeroContent />
