f800f3fc1fd4e4feba42fcb59432149ad1e5a380
aisle57 <truistdesigns@gmail.com>
2026-07-28 14:55:51 -0400

    Add favicon icons to metadata in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,14 @@ export const viewport: Viewport = {
   themeColor: "#030014",
 };
 
-export const metadata: Metadata = siteConfig;
+export const metadata: Metadata = {
+  ...siteConfig,
+  icons: {
+    icon: "/Favicon-new.jpg",
+    shortcut: "/Favicon-new.jpg",
+    apple: "/Favicon-new.jpg",
+  },
+};
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
