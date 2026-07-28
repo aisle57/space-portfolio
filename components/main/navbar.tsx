6032dbbacdd7db5f5e8d473f31a9d651dfbd4b7a
aisle57 <truistdesigns@gmail.com>
2026-07-28 13:04:51 -0400

    Update navbar shadow color to sky-900

--- components/main/navbar.tsx
+++ components/main/navbar.tsx
@@ -15,7 +15,7 @@ export const Navbar = () => {
   ];
 
   return (
-    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001427] backdrop-blur-md z-50 px-10">
+    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-sky-900/40 bg-[#03001427] backdrop-blur-md z-50 px-10">
       <div className="w-full h-full flex items-center justify-between m-auto px-[10px]">
         {/* Brand Name */}
         <Link href="/" className="flex items-center">
