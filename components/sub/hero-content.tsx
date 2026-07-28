5ab5eb934c856ac387c33c6ca8c7e309eca4c999
aisle57 <truistdesigns@gmail.com>
2026-07-28 14:37:19 -0400

    Refactor hero-content component layout and styles
    Removed SparklesIcon and adjusted class names for layout.

--- components/sub/hero-content.tsx
+++ components/sub/hero-content.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { SparklesIcon } from "@heroicons/react/24/solid";
 import { motion } from "framer-motion";
 import Image from "next/image";
 
@@ -18,19 +17,17 @@ export const HeroContent = () => {
       className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
     >
       <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
+        
         <motion.div
           variants={slideInFromTop}
-          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
+          className="text-sm text-sky-400 tracking-widest uppercase mb-2"
         >
-          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-          <h1 className="Welcome-text text-[13px]">
-            The Isotope Economy
-          </h1>
+          The Isotope Economy
         </motion.div>
 
         <motion.div
           variants={slideInFromLeft(0.5)}
-          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+          className="flex flex-col gap-6 mt-2 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
             Precision at the{" "}
